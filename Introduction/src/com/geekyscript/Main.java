aa0eea8cd5bfff3f4a4e1da29582ab4d87837b99
_isha_30 <choudhary.isha30@gmail.com>
2021-12-27 23:02:01 +0530

    Intro To HashMap

---
+++ Introduction/src/com/geekyscript/Main.java
@@ -0,0 +1,15 @@
+package com.geekyscript;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    HashMap<String, Integer> map = new HashMap<>();
+        map.put("India", 135);
+        map.put("Russia", 45);
+        map.put("US", 25);
+        map.put("UK", 20);
+
+        System.out.println(map);
+    }
+}
