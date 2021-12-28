d9bdb838cbf34d3243508d9414c6557e0739235c
_isha_30 <choudhary.isha30@gmail.com>
2021-12-28 23:59:29 +0530

    Longest Consecutive

---
+++ LongestConsecutiveSequence/src/com/geekyscript/Main.java
@@ -0,0 +1,48 @@
+package com.geekyscript;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n1 = sc.nextInt();
+        int[] arr = new int[n1];
+        System.out.print("Enter elements of array: ");
+        for(int i = 0; i<arr.length; i++)
+            arr[i] = sc.nextInt();
+
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int val : arr)
+            map.put(val, true);
+
+        for(int val : arr){
+            if(map.containsKey(val-1))
+                map.put(val, false);
+        }
+
+        int msp = 0;
+        int ml = 0;
+        for(int val : arr){
+            if(map.get(val)==true){
+                int tl = 1;
+                int tsp = val;
+
+                while(map.containsKey(tsp+tl)){
+                    tl++;
+                }
+
+                if(tl > ml){
+                    msp = tsp;
+                    ml = tl;
+                }
+            }
+        }
+
+        for(int i = 0; i<ml; i++){
+            System.out.println(msp+1);
+        }
+    }
+}
