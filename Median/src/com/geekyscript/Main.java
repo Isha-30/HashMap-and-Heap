9aae2753220ccbf4bc996dc8985501c6fee03600
_isha_30 <choudhary.isha30@gmail.com>
2021-12-30 21:51:33 +0530

    Find median of Priority Queue

---
+++ Median/src/com/geekyscript/Main.java
@@ -0,0 +1,34 @@
+package com.geekyscript;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        MedianPriorityQueue qu = new MedianPriorityQueue();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("add")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                qu.add(val);
+            } else if (str.startsWith("remove")) {
+                int val = qu.remove();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("peek")) {
+                int val = qu.peek();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(qu.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
